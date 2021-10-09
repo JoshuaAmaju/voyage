b6f2a9656667ce0ccbabf3e4129f02ca9b734ea7
Joshua Amaju <princejosh0007@gmail.com>
2021-10-09 19:27:47 +0100

    add store config

---
+++ src/zustand/player.store.ts
@@ -0,0 +1,19 @@
+import create from "zustand";
+
+type Context = {
+  file?: File;
+  currentTime: number;
+};
+
+type Actions = {
+  set(args: Partial<Context>): void;
+};
+
+type State = Context & Actions;
+
+const useStore = create<State>((set, get) => ({
+  currentTime: 0,
+  set: (args) => set({ ...get(), ...args }),
+}));
+
+export default useStore;
