b2df259e6d8d0e51270424f0994bee218ddee765
Joshua Amaju <princejosh0007@gmail.com>
2021-10-09 21:16:42 +0100

    add floater transition on page change

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,30 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./modules/Home";
 import Player from "./modules/Player";
 import { useManager } from "./context/Manager";
+import Floater from "./modules/Floater";
 
 function App() {
-  const { enterFull, isFloating } = useManager();
+  const { isFloating } = useManager();
 
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/player">
-          <Player />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/player">
+            <Player />
+          </Route>
+        </Switch>
+
+        {isFloating && (
+          <div className="w-96 h-60 fixed right-0 bottom-0 m-4 rounded">
+            <Floater />
+          </div>
+        )}
+      </BrowserRouter>
+    </>
   );
 }
 
