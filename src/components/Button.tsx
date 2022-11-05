9482cc7afb4397ce9265384252de175bc8d164d3
Joshua Amaju <princejosh0007@gmail.com>
2022-11-05 01:40:09 +0100

    chore: add style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,8 @@
 import { styled, Button as MUIButton } from "@material-ui/core";
 
 const Button = styled(MUIButton)((t) => ({
+  borderRadius: "0.4rem",
   backgroundColor: "#f9184c",
-  borderRadius: t.theme.shape.borderRadius,
   "&:hover": {
     backgroundColor: "#f9184c",
   },
